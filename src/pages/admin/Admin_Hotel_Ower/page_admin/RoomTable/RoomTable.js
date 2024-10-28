7089f342e3de79749ac3579b458de606bb475c84
Hvu <vunhpc06500@fpt.edu.vn>
2024-10-28 20:50:53 +0700

    truy cap trai phep

--- src/pages/admin/Admin_Hotel_Ower/page_admin/RoomTable/RoomTable.js
+++ src/pages/admin/Admin_Hotel_Ower/page_admin/RoomTable/RoomTable.js
@@ -1,7 +1,5 @@
-import React, { useState ,useRef,useEffect} from "react";
+import React, { useState} from "react";
 import { Table } from "react-bootstrap";
-import { useParams } from 'react-router-dom';
-import axios from 'axios';
 import '../../../../../assets/css/admin/css/chart.css'; // Custom CSS
 
 const RoomTable = () => {
