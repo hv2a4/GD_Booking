04cfa30bbe56dfa4fcfc7a2c7562591e338aeee3
anh le minh khoi dog <mjkkhoi1@gmail.com>
2024-11-08 18:59:45 +0700

    kk

--- src/components/home/Carousel.js
+++ src/components/home/Carousel.js
@@ -1,12 +1,19 @@
-import React, { useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import { carouselData } from "../data/Data";
+import { getImageHotel } from "../../services/admin/home-info-service";
+import Alert from "../../config/alert";
 
 export default function Carousel() {
   const sliderRef = useRef(null);
+  const [image, setImage] = useState([]);
+  const [alert, setAlert] = useState(null);
 
+  useEffect(() => {
+    handleImageHotel();
+  }, []);
   const next = () => {
     if (sliderRef.current) {
       sliderRef.current.slickNext();
@@ -26,9 +33,24 @@ export default function Carousel() {
     slidesToShow: 1,
     slidesToScroll: 1,
   };
+  const handleImageHotel = async () => {
+    try {
+      const data = await getImageHotel();
+      if (!data) {
+        setAlert({ type: "error", title: "Lỗi không tải được ảnh" });
+      } else {
+        setImage(data);
+      }
+    } catch (error) {
+      setAlert({ type: "error", title: error });
+    }
+
+
+  }
   return (
     <>
       <div className="container-fluid p-0 mb-5">
+        {alert && <Alert type={alert.type} title={alert.title} />}
         <div
           id="header-carousel"
           className="carousel slide"
@@ -36,28 +58,28 @@ export default function Carousel() {
         >
           <div className="carousel-inner">
             <Slider ref={sliderRef} {...settings}>
-              {carouselData.map((val, index) => (
+            {image.map((val, index) => (
                 <div className="carousel-item" key={index}>
-                  <img className="w-100" src={val.img} alt="Image" />
+                  <img className="w-100" src={val.imageName} alt="Image" height={700}/>
                   <div className="carousel-caption d-flex flex-column align-items-center justify-content-center">
                     <div className="p-3" style={{ maxWidth: "700px" }}>
                       <h6 className="section-title text-white text-uppercase mb-3 animated slideInDown">
-                        {val.subtitle}
+                        Cuộc sống xa hoa
                       </h6>
                       <h1 className="display-3 text-white mb-4 animated slideInDown">
-                        {val.title}
+                        Khám phá một khách sạn sang trọng
                       </h1>
                       <a
                         href=""
                         className="btn btn-primary py-md-3 px-md-5 me-3 animated slideInLeft"
                       >
-                        {val.btn1}
+                        Phòng của chúng tôi
                       </a>
                       <a
                         href=""
                         className="btn btn-light py-md-3 px-md-5 animated slideInRight"
                       >
-                        {val.btn2}
+                        Đặt phòng
                       </a>
                     </div>
                   </div>
--- src/components/home/Service.js
+++ src/components/home/Service.js
@@ -1,12 +1,31 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import CommonHeading from "../common/CommonHeading";
-import { services } from "../data/Data";
+import { getAllServiceHotel } from "../../services/client/home";
+import Alert from "../../config/alert";
 
 export default function Services() {
-  // const [services, setService] = useState([]);
+  const [services, setService] = useState([]);
+  const [alert, setAlert] = useState(null);
+
+  useEffect(() => {
+    handleServiceHotel();
+  }, []);
+  const handleServiceHotel = async () => {
+    try {
+      const data = await getAllServiceHotel();
+      if (data) {
+        setService(data);
+      } else {
+        setAlert({ type: "error", title: "Lỗi không tải được dữ liệu" });
+      }
+    } catch (error) {
+      setAlert({ type: "error", title: error });
+    }
+  }
   return (
     <>
       <div className="container-xxl py-5">
+        {alert && <Alert type={alert.type} title={alert.title} />}
         <div className="container">
           <div className="text-center wow fadeInUp" data-wow-delay="0.1s">
             <CommonHeading
@@ -16,16 +35,16 @@ export default function Services() {
             />
           </div>
           <div className="row g-4">
-            {services.map((item, index) => (
+            {(services.length > 6 ? services.slice(0,6):services).map((item, index) => (
               <div className="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.1s">
                 <a className="service-item rounded text-orange" href="">
                   <div className="service-icon bg-transparent border rounded p-1">
                     <div className="w-100 h-100 border rounded d-flex align-items-center justify-content-center">
-                      {item.icon}
+                      <i className={item?.icon}></i>
                     </div>
                   </div>
-                  <h5 className="mb-3">{item.name}</h5>
-                  <p className="text-body mb-0">{item.discription}</p>
+                  <h5 className="mb-3">{item?.serviceHotelName}</h5>
+                  <p className="text-body mb-0">{item?.hotel?.descriptions}</p>
                 </a>
               </div>
             ))}
