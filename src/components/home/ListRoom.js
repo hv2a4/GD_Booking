a703fdec1dc81c4dc59e82716f9dcf9ccbdea4d9
Hvu <vunhpc06500@fpt.edu.vn>
2024-12-01 13:03:03 +0700

    d

--- src/components/home/ListRoom.js
+++ src/components/home/ListRoom.js
@@ -50,6 +50,7 @@ export default function ListRoom() {
     const roomsPerPage = 3; // Số lượng phòng trên mỗi trang
     const [currentPageIndex, setCurrentPageIndex] = useState(1); // Trang hiện tại
     const location = useLocation();
+    const [rooms,setRooms] = useState([]);
     // Tính toán tổng số trang
     const totalPageCount = Math.ceil(selectedRooms.length / roomsPerPage);
 
@@ -70,10 +71,24 @@ export default function ListRoom() {
             const res = await getListRoom(currentPage - 1, pageSize); // Lấy phòng dựa trên trang hiện tại
             setTypeRoom(res.rooms);
             setTotalPages(res.totalPages); // Tổng số trang từ API danh sách phòng
+            setRooms(res.rooms);
         } catch (error) {
             console.log("Lỗi API trả về: ", error);
         }
     };
+
+    useEffect(() => {
+        // Lọc dữ liệu để lấy danh sách unique typeRoomName
+        const uniqueTypeRooms = rooms.reduce((acc, room) => {
+            if (!acc.some(item => item.typeRoomName === room.typeRoomName)) {
+                acc.push(room);
+            }
+            return acc;
+        }, []);
+        console.log(uniqueTypeRooms);
+        
+        setTypeRoom(uniqueTypeRooms);
+    }, [rooms]);
 
     // Effect để gọi API khi trang thay đổi
     useEffect(() => {
@@ -240,83 +255,117 @@ export default function ListRoom() {
                 <BookingFillter onFilter={handleDataFilter} onSendDates={handleDatesFromChild} />
                 <CommonHeading heading="Phòng của chúng tôi" title="Phòng" subtitle="Khám phá" />
                 {renderAlert}
+
                 <div className="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-4">
                     {typeRoom.length === 0 ? (
                         <div className="col-12 text-center">Không có phòng nào để hiển thị.</div>
                     ) : (
                         typeRoom.map((item, key) => (
-                            <div className="col-lg-4 col-md-6 col-sm-12 wow fadeInUp" data-wow-delay="0.1s" key={key}>
-                                <div className="room-item shadow rounded overflow-hidden">
-                                    <div className="position-relative">
-                                        <img className="img-fluid w-100" style={{ height: '271px' }} src={item?.imageList?.[0]} alt={item?.typeRoomName || "Room Image"} />
-                                        <div className="d-flex align-items-center position-absolute start-0 top-100 translate-middle-y ms-4">
-                                            <small className="bg-warning text-white rounded py-1 px-3">
-                                                {new Intl.NumberFormat("vi-VN", { style: "currency", currency: "VND" }).format(item?.price)}/ <strong>Ngày</strong>
-                                            </small>
+                            <div className="col-lg-12 col-md-12 col-sm-12 wow fadeInUp" data-wow-delay="0.1s" key={key}>
+                                <div className="row border">
+                                    {/* Phần hiển thị phòng */}
+                                    <div className="room-item rounded overflow-hidden col-md-5" style={{ padding: '0' }}>
+                                        <div className="position-relative">
+                                            <img
+                                                className="img-fluid w-100"
+                                                style={{ height: '271px' }}
+                                                src={item?.imageList?.[0]}
+                                                alt={item?.typeRoomName || "Room Image"}
+                                            />
+                                            <div className="d-flex align-items-center position-absolute start-0 top-100 translate-middle-y ms-4">
+                                                <small className="bg-warning text-white rounded py-1 px-3" style={{ fontWeight: 'bold' }}>
+                                                    {new Intl.NumberFormat("vi-VN", { style: "currency", currency: "VND" }).format(item?.price)}/ <strong>Ngày</strong>
+                                                </small>
+                                            </div>
+                                        </div>
+
+                                        {/* Thông tin phòng */}
+                                        <div className="p-4 mt-2" style={{borderRight: '1px soild'}}>
+                                            <h5 className="mb-3"><strong>{item?.typeRoomName}</strong></h5>
+                                            <div className="mb-3" style={{ fontSize: "1rem" }}>
+                                                <strong>Sức chứa: </strong>Tối đa {item?.guestLimit} người
+                                            </div>
+
+                                            {/* Tiện nghi phòng */}
+                                            <div className="mb-3">
+                                                <strong>Tiện nghi phòng:</strong>
+                                                <div className="d-flex flex-wrap mt-2">
+                                                    {item?.amenitiesDetails?.map((amenity, idx) => (
+                                                        <small
+                                                            className="border-end me-3 pe-3 d-flex align-items-center mb-2"
+                                                            key={idx}
+                                                            style={{ fontSize: "1.2rem" }}
+                                                        >
+                                                            {amenityIcons[amenity]}
+                                                            <span
+                                                                style={{
+                                                                    fontSize: "1rem",
+                                                                    maxWidth: "82px",
+                                                                    whiteSpace: "nowrap",
+                                                                    overflow: "hidden",
+                                                                    textOverflow: "ellipsis"
+                                                                }}
+                                                                title={amenity}
+                                                            >
+                                                                &nbsp;{amenity}
+                                                            </span>
+                                                        </small>
+                                                    ))}
+                                                    {item?.amenitiesDetails?.length > 3 && (
+                                                        <small
+                                                            className="more-amenities"
+                                                            style={{ fontSize: "1.2rem", color: "#FEA116" }}
+                                                            title="Click to see more amenities"
+                                                        >
+                                                            &nbsp;...
+                                                        </small>
+                                                    )}
+                                                </div>
+                                            </div>
+
+                                            {/* Nút hành động */}
+                                            <div className="d-flex justify-content-end">
+                                                <Button
+                                                    className="btn btn-sm btn-primary rounded py-2 px-4"
+                                                    onClick={() => getDataDetail(item.roomId)}
+                                                >
+                                                    Xem chi tiết
+                                                </Button>
+                                            </div>
                                         </div>
                                     </div>
-                                    <div className="p-4 mt-2">
-                                        <div className="d-flex justify-content-between mb-3">
-                                            <h5 className="mb-0">{item?.typeRoomName} ({item?.roomName})</h5>
-                                        </div>
-                                        <div className="d-flex justify-content-between mb-3">
-                                            <div className="ps-2" style={{ fontSize: "1rem" }}>Tối đa {item?.guestLimit} người</div>
-                                        </div>
-                                        <div className="d-flex mb-3">
-                                            {item?.amenitiesDetails?.slice(0, 3).map((amenity, idx) => (
-                                                <small
-                                                    className="border-end me-3 pe-3 d-flex align-items-center"
-                                                    key={idx}
-                                                    style={{ fontSize: "1.2rem" }}
-                                                >
-                                                    {amenityIcons[amenity]}
-                                                    <span
-                                                        style={{
-                                                            fontSize: "1rem",
-                                                            maxWidth: "82px",
-                                                            whiteSpace: "nowrap",
-                                                            overflow: "hidden",
-                                                            textOverflow: "ellipsis"
-                                                        }}
-                                                        title={amenity}
-                                                    >
-                                                        &nbsp;{amenity}
-                                                    </span>
-                                                </small>
-                                            ))}
-                                            {item?.amenitiesDetails?.length > 3 && (
-                                                <small
-                                                    className="more-amenities"
-                                                    style={{ fontSize: "1.2rem", color: "#FEA116" }}
-                                                    title="Click to see more amenities"
-                                                >
-                                                    &nbsp;...
-                                                </small>
-                                            )}
-                                        </div>
-                                        <p
-                                            className="text-body mb-3"
-                                            style={{
-                                                height: "80px",
-                                                overflow: "hidden",
-                                                textOverflow: "ellipsis",
-                                                lineHeight: "1.5",
-                                            }}
-                                        >
-                                            {item.description}
-                                        </p>
-                                        <div className="d-flex justify-content-between">
-                                            <Button className="btn btn-sm btn-primary rounded py-2 px-4" onClick={() => getDataDetail(item.roomId)}>
-                                                Chi tiết
-                                            </Button>
-                                            <Button
-                                                className="btn btn-primary"
-                                                onClick={() => handleSelectRoom(item)}
-                                            >
-                                                {selectedRooms.some((r) => r.roomId === item.roomId)
-                                                    ? "Bỏ chọn"
-                                                    : "Chọn"}
-                                            </Button>
+
+                                    {/* Danh sách đặt phòng */}
+                                    <div className="col-md-7">
+                                        <div className="card border-0">
+                                            <div className="card-header text-center bg-white">
+                                                <h3>Danh sách phòng</h3>
+                                            </div>
+                                            <div className="card-body bg-white">
+                                                <div className="d-flex flex-wrap justify-content-center">
+                                                    {typeRoom.map((roomItem, key) => (
+                                                        <Button
+                                                            key={key}
+                                                            className="btn btn-outline-primary m-3 shadow-none d-flex align-items-center justify-content-center"
+                                                            style={{
+                                                                width: '150px',
+                                                                height: '80px',
+                                                                fontWeight: 'bold',
+                                                                textAlign: 'center',
+                                                                padding: 0,
+                                                                backgroundColor: 'transparent',
+                                                                border: '2px solid #feaf39',
+                                                                transition: 'all 0.3s ease',
+                                                            }}
+                                                        >
+                                                            <i className="bi bi-door-open me-2" style={{ fontSize: '1.5rem' }}></i>
+                                                            <div style={{ fontSize: '1rem', wordWrap: 'break-word' }}>
+                                                                {roomItem?.roomName || `Phòng ${key + 1}`}
+                                                            </div>
+                                                        </Button>
+                                                    ))}
+                                                </div>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
