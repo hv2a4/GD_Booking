42eb348c381841512af6a07bbedca53a8f7d3c11
nghia <nghialtpc06472@fpt.edu.vn>
2024-12-18 19:01:06 +0700

    add code

--- src/pages/account/profile/my-profile/apiUpload.js
+++ src/pages/account/profile/my-profile/apiUpload.js
@@ -6,7 +6,7 @@ const uploadProfiles = async (datas) => {
     try {
       const res = await request({
         method: "PUT", // Phương thức HTTP GET
-        path: `/api/account/updateAccount`, // Truyền accountId thẳng trên URL
+        path: `/api/account/updateAccountCustomer`, // Truyền accountId thẳng trên URL
         data:datas
         //headers: { Authorization: `Bearer ${token}` }, // Thêm token vào header nếu cần
       });
